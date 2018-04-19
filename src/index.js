1267e06652c8064c41df86891ca609296a16d8b6
Michael Graham <38390185+michaeldgraham@users.noreply.github.com>
2018-04-18 18:26:58 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,90 @@
+import fetch from 'cross-fetch';
+import { print } from 'graphql';
+import { Binding  } from 'graphql-binding';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloLink, Observable } from 'apollo-link';
+import { makeRemoteExecutableSchema, introspectSchema } from 'graphql-tools';
+
+export const neo4jGraphQLBinding = (opt) => {
+  const { driver, typeDefs } = opt;
+  neo4jGraphqlIdl(driver, typeDefs);
+  const neo4jSchema = makeRemoteExecutableSchema({
+    schema: typeDefs,
+    link: neo4jGraphqlLink(driver)
+  });
+  return new Binding({
+    schema: neo4jSchema
+  });
+};
+
+export const neo4jExecute = async (params, ctx, info) => {
+  switch(info.parentType.name) {
+    case "Mutation": {
+      return await neo4jMutation(params, ctx, info);
+    }
+    case "Query": {
+      return await neo4jQuery(params, ctx, info);
+    }
+    case 'Subscription': {
+      throw Error(`Subscriptions not yet supported by neo4j-graphql-binding`);
+    }
+  }
+  throw Error(`Unsupported value for parentType.name`);
+}
+
+const neo4jMutation = async (params, ctx, info) => {
+  return await ctx.neo4j.mutation[info.fieldName](params, ctx, info);
+}
+
+const neo4jQuery = async (params, ctx, info) => {
+  return await ctx.neo4j.query[info.fieldName](params, ctx, info);
+}
+
+const neo4jGraphqlLink = (driver) => {
+  return new ApolloLink((operation, forward) => {
+    return new Observable(observer => {
+      return neo4jGraphqlRequest(driver, observer, operation);
+    });
+  });
+};
+
+const transformVariables = (params) => {
+  let transformed = [];
+  let transformedParam = "";
+  let param = '';
+  let p = 0;
+  const keys = Object.keys(params);
+  const len = keys.length;
+  for(; p < len; ++p) {
+    param = keys[p];
+    transformed.push(`${param}: {${param}}`);
+  }
+  return transformed.join(',\n');
+};
+
+const neo4jGraphqlIdl = async (driver, schema) => {
+  const session = driver.session();
+  await session
+    .run('CALL graphql.idl({schema})', {schema: schema})
+    .then(function (result) {
+      session.close();
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+};
+
+const neo4jGraphqlRequest = (driver, observer, operation) => {
+  const session = driver.session();
+  session.run(`CALL graphql.execute("${print(operation.query)}", {${transformVariables(operation.variables)}})`, operation.variables)
+  .then(result => {
+    session.close();
+    observer.next({
+      data: result.records[0]._fields[0]
+    });
+    observer.complete();
+  })
+  .catch(error => {
+    observer.error(error);
+  });
+};
